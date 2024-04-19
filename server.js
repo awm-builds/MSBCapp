30c043b528c985afaf1021ff345fc3e03fd8ea8a
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-19 18:27:14 -0400

    -Added override middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 require('dotenv').config();
 
@@ -14,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use(logger('dev'));
 app.use(express.json());
