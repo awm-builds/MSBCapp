f27c906bf2c96c9afaeed3b4a03c20dd6a094222
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-25 13:09:52 -0400

    -Added review schema to comics schema

--- models/comic.js
+++ models/comic.js
@@ -5,6 +5,21 @@ const reviewsSchema = new Schema({
     
 });
 
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  }
+}, {
+  timestamps: true
+});
+
 const comicSchema = new Schema({
     title: String,
     apiId: {
